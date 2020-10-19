3718a55c3e4df63a640c6dc60173cedcecfbf0ce
Week-3nd <theophile.dreux.auditeur@lecnam.net>
2020-10-19 17:09:28 +0200

    Début du scripting

---
+++ Assets/SimpleMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class SimpleMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //Vitesse de l'animation
+    [SerializeField]
+    private Vector3 movePerSec = new Vector3(1, 0, 0);
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform t = GetComponent<Transform>();
+        t.position += movePerSec * Time.deltaTime;
+    }
+}
